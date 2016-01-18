1d5d0ac43a0c5c30e1f42c99caec40575d676e5f
Simon Wirtz <simon.wirtz1@gmail.com>
2016-01-18 21:25:37 +0100

    Erweiterungen für Vortrag

--- src/de/ndesign/swirtz/javaeight/lambdas/SAMTransformationExample.java
+++ src/de/ndesign/swirtz/javaeight/lambdas/SAMTransformationExample.java
@@ -1,6 +1,9 @@
 package de.ndesign.swirtz.javaeight.lambdas;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * author: Simon
@@ -26,11 +29,23 @@ public class SAMTransformationExample {
             }
         };
 
-        Comparator<String> compareByLengthLambda = (o1, o2) -> Integer.compare(o1.length(), o2.length());
         Comparator<String> compareByLengthLambda2 = (final String o1, final String o2) -> {
             return Integer.compare(o1.length(), o2.length());
         };
 
+        //Type Inference und Kurzschreibweise
+        Comparator<String> compareByLengthLambda = (o1, o2) -> Integer.compare(o1.length(), o2.length());
 
+        //Lambdas als Parameter und Rückgabewert
+        final List<String> names = Arrays.asList("Uwe", "Christoph", "Daniela", "Mandy");
+        Collections.sort(names, (o1, o2) -> Integer.compare(o1.length(), o2.length()));
+        System.out.println(names);
+        //Lamdba als Rückgabe von Methode, List sort-Methode, Methoden wie reversed() ab 1.8
+        names.sort(getStringComparator().reversed());
+        System.out.println(names);
+    }
+
+    private static Comparator<String> getStringComparator() {
+        return (o1, o2) -> Integer.compare(o1.length(), o2.length());
     }
 }
